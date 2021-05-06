26a5d99a26d50f31a47455462d96d86aee0232f8
Mixone-FinallyHere <miguel.terol.93@gmail.com>
2021-05-06 15:01:10 +0200

    First revision. No further plans to modify.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ValheimModTemplate
 Valheim Mod Template
+
+## Steps
+
+1. Rename filenames
+2. Rename occurrences of ValheimModTemplate to ModName via notepad++ or other
+3. Ensure environment variable VALHEIM_DIR points to correct location
---
+++ ValheimModTemplate/ValheimModTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimModTemplate
+{
+    [BepInPlugin(ID, "Valheim Plus Build Expansion", version)]
+    public class ValheimModTemplate : BaseUnityPlugin
+    {
+        public const string ID = "";
+        public const string version = "0.0.0.0";     
+
+        public Harmony harmony;
+        
+        public static BepInEx.Logging.ManualLogSource harmonyLog;
+
+        public void Awake()
+        {
+            harmony = new Harmony(ID);
+            harmony.PatchAll();
+            harmonyLog = Logger;
+
+            harmonyLog.LogDebug("ValheimModTemplate loaded.");
+        }
+    }
+
+    #region Utils
+
+    #endregion
+
+    #region Transpilers
+
+    #endregion
+
+    #region Patches 
+
+    #endregion
+}
